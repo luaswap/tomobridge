da5749465bb8ec83094033560539565f2cbc4103
hka <hoangkianh31592@gmail.com>
2020-02-12 23:26:42 +0700

    Add HTML for Wrap History

--- app/components/WrapHistory.vue
+++ app/components/WrapHistory.vue
@@ -65,48 +65,171 @@
                 </multiselect>
             </b-col>
         </b-row>
-        <b-row class="wrap-history__row">
+        <b-row class="wrap-history__row wrap-history__row--scroll">
             <b-col
                 cols="12"
-                md="5">
+                md="6"
+                lg="5">
                 <div class="wrap-history__wallet">
                     <h4 class="wrap-history__title">ETH wallet</h4>
                     <a
                         href="#"
                         class="wrap-history__address">bnb1s3f8vxaqum3pft6cefyn99px8wq6uk3jdtyarn</a>
                     <div class="wrap-history__wallet-info">
-                        <p class="wrap-history__balance">
+                        <p>
                             <i class="tb-wallet"/>
                             <span>Total balance: 234 BTC</span>
                         </p>
-                        <p class="wrap-history__txs">
+                        <p>
                             <i class="tb-swap"/>
                             <span>Transactions: 10,290 txs</span>
                         </p>
                     </div>
                 </div>
+                <div class="wrap-history__txs">
+                    <div class="wrap-history__txs-header">
+                        <h4 class="wrap-history__title">Transactions</h4>
+                        <a
+                            href="#"
+                            class="wrap-history__link">View all
+                            <i class="tb-long-arrow-right"/>
+                        </a>
+                    </div>
+                    <ul class="wrap-history__txs-list">
+                        <li class="wrap-history__txs-item">
+                            <p class="wrap-history__tx-row">TX#1:
+                                <span class="wrap-history__tx-hash">
+                                    0cf90s8f90d8f90ds8f90ds8f90sd8f98dsf908ds90f8
+                                </span>
+                            </p>
+                            <p class="wrap-history__tx-row">
+                                <span class="wrap-history__tx-from">
+                                    <span class="wrap-history__tx-text">From: </span>
+                                    <span class="wrap-history__tx-addr">0x9ffd9g8545435…</span>
+                                </span>
+                                <span class="wrap-history__tx-date">Dec-10-2019 14:24:29</span>
+                            </p>
+                            <p class="wrap-history__tx-row">
+                                <span class="wrap-history__tx-to">
+                                    <span class="wrap-history__tx-text">To: </span>
+                                    <span class="wrap-history__tx-addr">0x9ffd9g8545435…</span>
+                                </span>
+                                <span class="wrap-history__tx-qty">
+                                    <i class="tb-arrow-left"/>
+                                    5ETH
+                                </span>
+                            </p>
+                        </li>
+                        <li class="wrap-history__txs-item">
+                            <p class="wrap-history__tx-row">TX#1:
+                                <span class="wrap-history__tx-hash">
+                                    0cf90s8f90d8f90ds8f90ds8f90sd8f98dsf908ds90f8
+                                </span>
+                            </p>
+                            <p class="wrap-history__tx-row">
+                                <span class="wrap-history__tx-from">
+                                    <span class="wrap-history__tx-text">From: </span>
+                                    <span class="wrap-history__tx-addr">0x9ffd9g8545435…</span>
+                                </span>
+                                <span class="wrap-history__tx-date">Dec-10-2019 14:24:29</span>
+                            </p>
+                            <p class="wrap-history__tx-row">
+                                <span class="wrap-history__tx-to">
+                                    <span class="wrap-history__tx-text">To: </span>
+                                    <span class="wrap-history__tx-addr">0x9ffd9g8545435…</span>
+                                </span>
+                                <span class="wrap-history__tx-qty">
+                                    <i class="tb-arrow-right"/>
+                                    5ETH
+                                </span>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </b-col>
             <b-col
                 cols="2"
-                class="d-none d-md-block" />
+                class="d-none d-lg-block" />
             <b-col
                 cols="12"
-                md="5">
+                md="6"
+                lg="5">
                 <div class="wrap-history__wallet">
                     <h4 class="wrap-history__title">Wrapped ETH wallet</h4>
                     <a
                         href="#"
                         class="wrap-history__address">bnb1s3f8vxaqum3pft6cefyn99px8wq6uk3jdtyarn</a>
                     <div class="wrap-history__wallet-info">
-                        <p class="wrap-history__balance">
+                        <p>
                             <i class="tb-wallet"/>
                             <span>Total balance: 234 BTC</span>
                         </p>
-                        <p class="wrap-history__txs">
+                        <p>
                             <i class="tb-swap"/>
                             <span>Transactions: 10,290 txs</span>
                         </p>
                     </div>
+                </div>
+
+                <div class="wrap-history__txs">
+                    <div class="wrap-history__txs-header">
+                        <h4 class="wrap-history__title">Transactions</h4>
+                        <a
+                            href="#"
+                            class="wrap-history__link">View all
+                            <i class="tb-long-arrow-right"/>
+                        </a>
+                    </div>
+                    <ul class="wrap-history__txs-list">
+                        <li class="wrap-history__txs-item">
+                            <p class="wrap-history__tx-row">TX#1:
+                                <span class="wrap-history__tx-hash">
+                                    0cf90s8f90d8f90ds8f90ds8f90sd8f98dsf908ds90f8
+                                </span>
+                            </p>
+                            <p class="wrap-history__tx-row">
+                                <span class="wrap-history__tx-from">
+                                    <span class="wrap-history__tx-text">From: </span>
+                                    <span class="wrap-history__tx-addr">0x9ffd9g8545435…</span>
+                                </span>
+                                <span class="wrap-history__tx-date">Dec-10-2019 14:24:29</span>
+                            </p>
+                            <p class="wrap-history__tx-row">
+                                <span class="wrap-history__tx-to">
+                                    <span class="wrap-history__tx-text">To: </span>
+                                    <span class="wrap-history__tx-addr">0x9ffd9g8545435…</span>
+                                </span>
+                                <span class="wrap-history__tx-qty">
+                                    <i class="tb-arrow-left"/>
+                                    5ETH
+                                </span>
+                            </p>
+                        </li>
+                        <li class="wrap-history__txs-item">
+                            <p class="wrap-history__tx-row">TX#1:
+                                <span class="wrap-history__tx-hash">
+                                    0cf90s8f90d8f90ds8f90ds8f90sd8f98dsf908ds90f8
+                                </span>
+                            </p>
+                            <p class="wrap-history__tx-row">
+                                <span class="wrap-history__tx-from">
+                                    <span class="wrap-history__tx-text">From: </span>
+                                    <span class="wrap-history__tx-addr">0x9ffd9g8545435…</span>
+                                </span>
+                                <span class="wrap-history__tx-date">Dec-10-2019 14:24:29</span>
+                            </p>
+                            <p class="wrap-history__tx-row">
+                                <span class="wrap-history__tx-to">
+                                    <span class="wrap-history__tx-text">To: </span>
+                                    <span class="wrap-history__tx-addr">0x9ffd9g8545435…</span>
+                                </span>
+                                <span class="wrap-history__tx-qty">
+                                    <i class="tb-arrow-right"/>
+                                    5ETH
+                                </span>
+                            </p>
+                        </li>
+                    </ul>
                 </div>
             </b-col>
         </b-row>
