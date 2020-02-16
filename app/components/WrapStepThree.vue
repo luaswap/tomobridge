2f0ba0883c5fcc21c775edf3fe6c97dc3763abe4
hka <hoangkianh31592@gmail.com>
2020-02-16 18:18:34 +0700

    Add CSS for WrapStepThree.vue

--- app/components/WrapStepThree.vue
+++ app/components/WrapStepThree.vue
@@ -1,17 +1,38 @@
 <template>
-    <div style="text-align: center">
-        We are sending you...
-        <div>
-            1 WBTC to address
+    <b-container class="step-three text-center">
+        <h3 class="step-three__title">We are sending you...</h3>
+        <p class="step-three__subtitle">1 BTC to address</p>
+        <a
+            href="#"
+            class="step-three__address">
+            0x33c2E732ae7dce8B05F37B2ba0CFe14c980c4Dbe
+        </a>
+        <div class="step-three__progress">
+            <div class="progress-bar">
+                <div class="progress-bar__inner">
+                    <div
+                        class="progress-bar__bar"
+                        style="width: 46%;">
+                        <span class="progress-bar__number text-primary">46%</span>
+                    </div>
+                </div>
+                <span class="progress-bar__total">60 Blocks</span>
+            </div>
+            <div class="step-three__fee text-primary">
+                Fee: 1 TOMO
+            </div>
         </div>
-        0x33c2E732ae7dce8B05F37B2ba0CFe14c980c4Dbe
-        <div>
-            Transaction hash:
+        <div class="step-three__tx-hash">
+            <p>
+                Transaction hash:
+                <a
+                    href="#"
+                    class="step-three__tx-hash-link text-truncate">
+                    0x33c2E732ae7dce8B05F37B2ba0CFe14c980c4Dbeadasdasdasdas
+                </a>
+            </p>
         </div>
-        <div>
-            0x33c2E732ae7dce8B05F37B2ba0CFe14c980c4Dbeadasdasdasdas
-        </div>
-    </div>
+    </b-container>
 </template>
 
 <script>
