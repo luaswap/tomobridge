0dfe8e6799b3a27f0edbe95dbf3c735359d28532
pqv199x <pqv199x@hotmail.com>
2020-03-31 16:59:45 +0700

    add wrapping api, update config

---
+++ apis/wrap.js
@@ -0,0 +1,42 @@
+'use strict'
+const express = require('express')
+const config = require('config')
+const urljoin = require('url-join')
+const router = express.Router()
+const axios = require('axios')
+
+router.post('/getAddress', async function (req, res, next) {
+    try {
+        const wrapCoin = req.body.wrapCoin
+        const receiveAddress = req.body.receiveAddress
+        const url = urljoin(config.get('serverAPI'), '/address')
+        const result = await axios.post(url, {
+            coin: wrapCoin,
+            tomo: receiveAddress
+        })
+        return res.send(result.data)
+    } catch (error) {
+        return next(error)
+    }
+})
+
+router.get('/getTransaction/:wrapCoin/:receiveAddress',
+    async function (req, res, next) {
+        try {
+            const wrapCoin = req.params.wrapCoin
+            const receiveAddress = req.params.receiveAddress
+
+            const url = urljoin(
+                config.get('serverAPI'),
+                '/transactions/',
+                wrapCoin.toLowerCase(),
+                receiveAddress
+            )
+            const result = await axios.get(url)
+            return res.send(result.data)
+        } catch (error) {
+            return next(error)
+        }
+})
+
+module.exports = router
