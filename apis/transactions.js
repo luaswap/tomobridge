a2c196a4e416fd635c2b2adb22de2b87fbee9c92
pqv199x <pqv199x@hotmail.com>
2020-05-22 11:56:01 +0700

    update api validation

--- apis/transactions.js
+++ apis/transactions.js
@@ -6,68 +6,76 @@ const router = express.Router()
 const axios = require('axios')
 const { query, validationResult } = require('express-validator/check')
 
-router.get('/',
-    async function (req, res, next) {
-        try {
-            const page = req.query.page >= 1 ? req.query.page - 1 : 0 || 0
-            const limit = parseInt(req.query.limit / 2) || 100
-            const coin = req.query.coin || 'btc'
-            let response = {
-                mainTxs: [],
-                wrapTxs: [],
-                total: 0
-            }
-
-            const url = urljoin(
-                config.get('serverAPI'),
-                '/transactions',
-                `?coin=${coin}&page=${page}&limit=${limit}`
-            )
-            const result = await axios.get(url)
-            if (result && result.data) {
-                response.total = result.data.Total
-                await Promise.all(result.data.Data.map(item => {
-                    if (item.InTx.Status === 'DEPOSITED') {
-                        response.mainTxs.push(Object.assign(
-                            item.InTx,
-                            {
-                                CreatedAt: item.CreatedAt
-                            }
-                        ))
-                        response.wrapTxs.push(Object.assign(
-                            item.OutTx,
-                            {
-                                CreatedAt: item.CreatedAt
-                            }
-                        ))
-                    }
-                    if (item.InTx.Status === 'BURNED') {
-                        response.mainTxs.push(Object.assign(
-                            item.OutTx,
-                            {
-                                CreatedAt: item.CreatedAt
-                            }
-                        ))
-                        response.wrapTxs.push(Object.assign(
-                            item.InTx,
-                            {
-                                CreatedAt: item.CreatedAt
-                            }
-                        ))
-                    }
-                }))
-            }
-            return res.send(response)
-        } catch (error) {
-            return next(error)
+router.get('/', [
+    query('limit')
+        .isInt({ min: 0, max: 200 }).optional().withMessage("'limit' should greater than 0 and less than 200"),
+    query('page').isNumeric({ no_symbols: true }).optional().withMessage("'page' must be number"),
+    query('coin').exists().withMessage("'coin' is required, 'btc', 'eth' or 'usdt'")
+], async function (req, res, next) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return next(errors.array())
+    }
+    try {
+        const page = req.query.page >= 1 ? req.query.page - 1 : 0 || 0
+        const limit = parseInt(req.query.limit / 2) || 100
+        const coin = req.query.coin || 'btc'
+        let response = {
+            mainTxs: [],
+            wrapTxs: [],
+            total: 0
         }
-    })
+
+        const url = urljoin(
+            config.get('serverAPI'),
+            '/transactions',
+            `?coin=${coin}&page=${page}&limit=${limit}`
+        )
+        const result = await axios.get(url)
+        if (result && result.data) {
+            response.total = result.data.Total
+            await Promise.all(result.data.Data.map(item => {
+                if (item.InTx.Status === 'DEPOSITED') {
+                    response.mainTxs.push(Object.assign(
+                        item.InTx,
+                        {
+                            CreatedAt: item.CreatedAt
+                        }
+                    ))
+                    response.wrapTxs.push(Object.assign(
+                        item.OutTx,
+                        {
+                            CreatedAt: item.CreatedAt
+                        }
+                    ))
+                }
+                if (item.InTx.Status === 'BURNED') {
+                    response.mainTxs.push(Object.assign(
+                        item.OutTx,
+                        {
+                            CreatedAt: item.CreatedAt
+                        }
+                    ))
+                    response.wrapTxs.push(Object.assign(
+                        item.InTx,
+                        {
+                            CreatedAt: item.CreatedAt
+                        }
+                    ))
+                }
+            }))
+        }
+        return res.send(response)
+    } catch (error) {
+        return next(error)
+    }
+})
 
 router.get('/getWrapTxs', [
     query('limit')
-        .isInt({ min: 0, max: 200 }).optional().withMessage('limit should greater than 0 and less than 200'),
-    query('page').isNumeric({ no_symbols: true }).optional().withMessage('page must be number'),
-    query('address').exists().withMessage('Account address is required.')
+        .isInt({ min: 0, max: 200 }).optional().withMessage("'limit' should greater than 0 and less than 200"),
+    query('page').isNumeric({ no_symbols: true }).optional().withMessage("'page' must be number"),
+    query('address').exists().isLength({ min: 42, max: 42 }).withMessage("'address' is incorrect")
 ], async function (req, res, next) {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
@@ -94,9 +102,9 @@ router.get('/getWrapTxs', [
 
 router.get('/getUnwrapTxs', [
     query('limit')
-        .isInt({ min: 0, max: 200 }).optional().withMessage('limit should greater than 0 and less than 200'),
-    query('page').isNumeric({ no_symbols: true }).optional().withMessage('page must be number'),
-    query('address').exists().withMessage('Account address is required.')
+        .isInt({ min: 0, max: 200 }).optional().withMessage("'limit' should greater than 0 and less than 200"),
+    query('page').isNumeric({ no_symbols: true }).optional().withMessage("'page' must be number"),
+    query('address').exists().isLength({ min: 42, max: 42 }).withMessage("'address' is incorrect")
 ], async function (req, res, next) {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
