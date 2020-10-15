3e4a9e8f4915e5e7fe469977bb558a4f779a9b35
pqv199x <pqv199x@hotmail.com>
2020-10-15 14:37:45 +0700

    get config from api

--- apis/config.js
+++ apis/config.js
@@ -6,10 +6,22 @@ const BigNumber = require('bignumber.js')
 const WrapperAbi = require('../abis/WrapperAbi.json')
 const router = express.Router()
 const urljoin = require('url-join')
+const axios = require('axios')
+
+const getChainInfo = (chain) => {
+    switch (chain) {
+    case 'BTC':
+        return ['bitcoin', 'https://www.blockchain.com/btc']
+    case 'ETH':
+        return ['ethereum', 'https://etherscan.io']
+    default:
+        return ['', '']
+    }
+}
 
 router.get('/', async function (req, res, next) {
     let appConfig = {}
-    const swapCoin = JSON.parse(JSON.stringify(config.get('swapCoin')))
+    // const swapCoin = JSON.parse(JSON.stringify(config.get('swapCoin')))
     appConfig.blockchain = config.get('blockchain')
     appConfig.swapToken = config.get('swapToken')
     let tomoscanUrl = config.get('tomoscanUrl')
@@ -20,10 +32,30 @@ router.get('/', async function (req, res, next) {
 
     appConfig.GA = config.get('GA')
 
-    appConfig.swapCoin = await Promise.all(swapCoin.map(s => {
-        s.image = urljoin(config.get('tokenListAPI'), `${s.wrapperAddress.toLowerCase()}.png`)
-        return s
-    }))
+    const { data } = await axios.get(
+        urljoin(config.get('serverAPI'), 'tokens?page=1&limit=1000')
+    )
+    appConfig.swapCoin = data.Data.map(d => {
+        return {
+            name: d.symbol,
+            confirmations: d.confirmations,
+            decimals: d.decimals,
+            minimumWithdrawal: new BigNumber(d.min_deposit_value).div(10 ** d.decimals).toString(10),
+            image: urljoin(config.get('tokenListAPI'), `${d.wrap_smart_contract.toLowerCase()}.png`),
+            wrapperAddress: d.wrap_smart_contract,
+            tokenName: d.name,
+            network: getChainInfo(d.chain)[0],
+            explorerUrl: getChainInfo(d.chain)[1],
+            mainAddress: d.multisig_wallet,
+            tokenAddress: d.address,
+            coingecko_id: d.coingecko_id || ''
+        }
+    })
+
+    // appConfig.swapCoin = await Promise.all(swapCoin.map(s => {
+    //     s.image = urljoin(config.get('tokenListAPI'), `${s.wrapperAddress.toLowerCase()}.png`)
+    //     return s
+    // }))
 
     return res.json(appConfig)
 })
